2480d41b149f5d5f0170787cd73809a4a50faf28
eklemen <ejklemen@gmail.com>
2022-11-07 19:33:19 -0500

    formik

--- src/shared/CreateAlbumModal.tsx
+++ src/shared/CreateAlbumModal.tsx
@@ -12,8 +12,8 @@ import {
   Text,
   TextArea,
 } from 'native-base';
-import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import * as Yup from 'yup';
+import { Field, Form, Formik, FormikProps } from 'formik';
 
 interface Props {
   isOpen: boolean;
@@ -29,101 +29,101 @@ const NewAlbumSchema = Yup.object().shape({
 });
 
 function CreateAlbumModal({ isOpen }: Props) {
-  const {
-    control,
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm<FormInput>({
-    defaultValues: {
-      name: '',
-      description: '',
-    },
-    mode: 'all',
-  });
+  // const {
+  //   control,
+  //   handleSubmit,
+  //   register,
+  //   formState: { errors },
+  // } = useForm<FormInput>({
+  //   defaultValues: {
+  //     name: '',
+  //     description: '',
+  //   },
+  //   mode: 'all',
+  // });
   const onSubmit = (data: FormInput) => console.log(data);
   return (
     <Modal isOpen={isOpen} onClose={() => {}} size="xl">
-      <Modal.Content maxWidth="350">
-        <Modal.CloseButton />
-        <Modal.Body>
-          <Row mt={6}>
-            <Heading fontSize={26}>Create Album</Heading>
-          </Row>
-          <Column my={6}>
-            <Box>
-              <Stack m="4">
-                <FormControl isRequired isInvalid={'firstName' in errors}>
-                  <FormControl.Label>Album's Name</FormControl.Label>
-                  {/*<Input size="xl" placeholder="School Year 22-23" mb={4} />*/}
-                  <Controller
-                    name="name"
-                    control={control}
-                    rules={{
-                      required: true,
-                    }}
-                    defaultValue=""
-                    render={({ field: { onChange, value, onBlur } }) => (
+      <Formik
+        initialValues={{ name: '', description: '' } as FormInput}
+        onSubmit={(values) => {
+          console.log('values-------->', values);
+        }}
+        validationSchema={NewAlbumSchema}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          touched,
+        }: FormikProps<FormInput>) => (
+          <Modal.Content maxWidth="350">
+            <Modal.CloseButton />
+            <Modal.Body>
+              <Row mt={6}>
+                <Heading fontSize={26}>Create Album</Heading>
+              </Row>
+              <Column my={6}>
+                <Box>
+                  <Stack m="4">
+                    <FormControl isRequired>
+                      <FormControl.Label>Album's Name</FormControl.Label>
                       <Input
                         size="xl"
                         placeholder="School Year 22-23"
-                        mb={4}
-                        value={value}
-                        onChangeText={(value) => onChange(value)}
-                        {...register('name')}
+                        onChangeText={handleChange('name')}
+                        onBlur={handleBlur('name')}
+                        value={values.name}
                       />
-                    )}
-                  />
-                  <Text>{JSON.stringify(errors)}</Text>
-                  <FormControl.ErrorMessage>
-                    {errors.name?.message}
-                  </FormControl.ErrorMessage>
-                </FormControl>
-                <FormControl>
-                  <FormControl.Label>Description (optional)</FormControl.Label>
-                  {/*<TextArea*/}
-                  {/*  size="xl"*/}
-                  {/*  placeholder="Crafts and awards from Mrs. Smith's class."*/}
-                  {/*  numberOfLines={4}*/}
-                  {/*  autoCompleteType="on"*/}
-                  {/*  mb={4}*/}
-                  {/*/>*/}
-                  <Controller
-                    name="description"
-                    control={control}
-                    defaultValue=""
-                    render={({ field }) => (
+
+                      {errors.name && touched.name ? (
+                        <Text>{errors.name}</Text>
+                      ) : null}
+                    </FormControl>
+                    <FormControl>
+                      <FormControl.Label>
+                        Description (optional)
+                      </FormControl.Label>
+                      {/*<TextArea*/}
+                      {/*  size="xl"*/}
+                      {/*  placeholder="Crafts and awards from Mrs. Smith's class."*/}
+                      {/*  numberOfLines={4}*/}
+                      {/*  autoCompleteType="on"*/}
+                      {/*  mb={4}*/}
+                      {/*/>*/}
                       <TextArea
                         size="xl"
                         placeholder="Crafts and awards from Mrs. Smith's class."
                         numberOfLines={4}
                         autoCompleteType="on"
                         mb={4}
-                        {...field}
-                        value={field.value}
-                        onBlur={field.onBlur}
+                        onChangeText={handleChange('description')}
+                        onBlur={handleBlur('description')}
+                        value={values.description}
                       />
-                    )}
-                  />
-                </FormControl>
-              </Stack>
-            </Box>
-          </Column>
-        </Modal.Body>
-        <Modal.Footer borderTopWidth={0}>
-          <Button
-            colorScheme="secondary"
-            flex={1}
-            h={55}
-            onPress={handleSubmit as (values: any) => void}
-            isLoading={false}
-          >
-            <Text color="white" fontFamily="body" fontSize={22}>
-              Save
-            </Text>
-          </Button>
-        </Modal.Footer>
-      </Modal.Content>
+                    </FormControl>
+                  </Stack>
+                </Box>
+              </Column>
+            </Modal.Body>
+            <Modal.Footer borderTopWidth={0}>
+              <Button
+                colorScheme="secondary"
+                flex={1}
+                h={55}
+                onPress={handleSubmit}
+                isLoading={false}
+              >
+                <Text color="white" fontFamily="body" fontSize={22}>
+                  Save
+                </Text>
+              </Button>
+            </Modal.Footer>
+          </Modal.Content>
+        )}
+      </Formik>
     </Modal>
   );
 }
